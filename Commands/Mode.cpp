0a305f038ab0da9e6f8e77bfeb7155769f97baa3
cantasar <62062865+cantasar@users.noreply.github.com>
2025-02-14 14:52:00 +0300

    MODE +k -k command

--- Commands/Mode.cpp
+++ Commands/Mode.cpp
@@ -3,122 +3,142 @@
 
 void Server::mode(size_t client_index)
 {
-	// Check if client is connected
-	if (!clients[client_index].getConnected())
-	{
-		clients[client_index].message(":server 451 " + clients[client_index].getNickname() + " :You have not registered\r\n");
-		return;
-	}
-	
-	// Check if we have enough parameters (MODE <channel> <mode> <user>)
-	if (input.size() < 4)
-	{
-		clients[client_index].message(":server 461 " + clients[client_index].getNickname() + " MODE :Not enough parameters\r\n");
-		clients[client_index].message(":server 461 " + clients[client_index].getNickname() + " MODE :Useage MODE <channel> <mode> <user>\r\n");
-		return;
-	}
+    // Check if client is connected
+    if (!clients[client_index].getConnected())
+    {
+        clients[client_index].message(":server 451 " + clients[client_index].getNickname() + " :You have not registered\r\n");
+        return;
+    }
 
-	std::string nickname = clients[client_index].getNickname();
-	std::string channel_name = input[1];
-	std::string mode = input[2];
-	std::string user = input[3];
+    // Check parameters
+    if (input.size() < 3)
+    {
+        clients[client_index].message(":server 461 " + clients[client_index].getNickname() + " MODE :Not enough parameters\r\n");
+        clients[client_index].message(":server 461 " + clients[client_index].getNickname() + " MODE :Useage MODE <channel> <mode> [parameter]\r\n");
+        return;
+    }
 
-	// Check if channel name starts with '#'
-	if (channel_name[0] != '#')
-	{
-		clients[client_index].message(":server 476 " + clients[client_index].getNickname() + " " + channel_name + " :Invalid channel name\r\n");
-		return;
-	}
+    std::string nickname = clients[client_index].getNickname();
+    std::string channel_name = input[1];
+    std::string mode = input[2];
+    std::string parameter = "";
+    if (input.size() > 3)
+    {
+        parameter = input[3];
+    }
 
-	//+o - Give operator status
-	//-o - Remove operator status
+    // Validate channel name
+    if (channel_name[0] != '#')
+    {
+        clients[client_index].message(":server 476 " + clients[client_index].getNickname() + " " + channel_name + " :Invalid channel name\r\n");
+        return;
+    }
 
-	// Check if channel already exists
-	for (std::vector<Channel>::iterator it = channels.begin(); it != channels.end(); ++it)
-	{
-		if (it->getName() == channel_name)
-		{
-			// Check if client is not in the channel
-			if (it->isClientInChannel(nickname) == false)
-			{
-				clients[client_index].message(":server 442 " + clients[client_index].getNickname() + " " + channel_name + " :You're not on that channel\r\n");
-				return;
-			}
+    // Find channel
+    bool channel_found = false;
+    for (std::vector<Channel>::iterator it = channels.begin(); it != channels.end(); ++it)
+    {
+        if (it->getName() == channel_name)
+        {
+            channel_found = true;
+            // Validate client permissions
+            if (!it->isClientInChannel(nickname))
+            {
+                clients[client_index].message(":server 442 " + clients[client_index].getNickname() + " " + channel_name + " :You're not on that channel\r\n");
+                return;
+            }
 
-			// Check if client is an operator
-			if (it->isOperator(clients[client_index]) == false)
-			{
-				clients[client_index].message(":server 482 " + clients[client_index].getNickname() + " " + channel_name + " :You're not channel operator\r\n");
-				return;
-			}
+            if (!it->isOperator(clients[client_index]))
+            {
+                clients[client_index].message(":server 482 " + clients[client_index].getNickname() + " " + channel_name + " :You're not channel operator\r\n");
+                return;
+            }
 
-			// Check if user is in the channel
-			if (it->isClientInChannel(user) == false)
-			{
-				clients[client_index].message(":server 441 " + clients[client_index].getNickname() + " " + user + " " + channel_name + " :They aren't on that channel\r\n");
-				return;
-			}
+            std::vector<Client> channel_clients = it->getClients();
+            std::string mode_message;
 
-			// // Check if user is an operator
-			// if (it->isOperator(user) == true)
-			// {
-			// 	clients[client_index].message(":server 482 " + clients[client_index].getNickname() + " " + channel_name + " :You're not channel operator\r\n");
-			// 	return;
-			// }
+            // Handle different modes
+            if (mode == "+o")
+            {
+                if (parameter.empty())
+                {
+                    clients[client_index].message(":server 461 " + clients[client_index].getNickname() + " MODE :Not enough parameters\r\n");
+                    clients[client_index].message(":server 461 " + clients[client_index].getNickname() + " MODE :Useage MODE <channel> +o <nickname>\r\n");
+                    return;
+                }
 
-			// Give operator status
-			if (mode == "+o")
-			{
-				std::vector<Client> channel_clients = it->getClients();
-				for (std::vector<Client>::iterator client_it = channel_clients.begin(); client_it != channel_clients.end(); ++client_it)
-				{
-					if (client_it->getNickname() == user)
-					{
-						it->addOperator(*client_it);
-						break;
-					}
-				}
-				std::string mode_message = ":" + clients[client_index].getNickname() + "!" + 
-				clients[client_index].getUsername() + "@" + 
-				clients[client_index].getIp_address() + 
-				" MODE " + channel_name + " +o " + user + "\r\n";
-				
-				// Send mode message to all clients in the channel
-				for (std::vector<Client>::iterator channel_client_it = channel_clients.begin(); channel_client_it != channel_clients.end(); ++channel_client_it)
-				{
-					channel_client_it->message(mode_message);
-				}
-			}
-			// Remove operator status
-			else if (mode == "-o")
-			{
-				std::vector<Client> channel_clients = it->getClients();
-				std::vector<Client> channel_ops = it->getOperators();
-				for (std::vector<Client>::iterator op_it = channel_ops.begin(); op_it != channel_ops.end(); ++op_it)
-				{
-					if (op_it->getNickname() == user)
-					{
-						it->removeOperator(*op_it);
-						break;
-					}
-				}
-				std::string mode_message = ":" + clients[client_index].getNickname() + "!" + 
-											clients[client_index].getUsername() + "@" + 
-											clients[client_index].getIp_address() + 
-											" MODE " + channel_name + " -o " + user + "\r\n";
-				// Send mode message to all clients in the channel
-				for (std::vector<Client>::iterator channel_client_it = channel_clients.begin(); channel_client_it != channel_clients.end(); ++channel_client_it)
-				{
-					channel_client_it->message(mode_message);
-				}
-			}
-			else
-			{
-				clients[client_index].message(":server 472 " + clients[client_index].getNickname() + " " + mode + " :Unknown MODE flag\r\n");
-				return;
-			}
-			return;
-		}
-	}
+                if (!it->isClientInChannel(parameter))
+                {
+                    clients[client_index].message(":server 441 " + clients[client_index].getNickname() + " " + parameter + " " + channel_name + " :They aren't on that channel\r\n");
+                    return;
+                }
+
+                for (std::vector<Client>::iterator client_it = channel_clients.begin(); client_it != channel_clients.end(); ++client_it)
+                {
+                    if (client_it->getNickname() == parameter)
+                    {
+                        it->addOperator(*client_it);
+                        break;
+                    }
+                }
+                mode_message = ":" + clients[client_index].getNickname() + "!" + clients[client_index].getUsername() + "@" + clients[client_index].getIp_address() + " MODE " + channel_name + " +o " + parameter + "\r\n";
+            }
+            else if (mode == "-o")
+            {
+                if (parameter.empty())
+                {
+                    clients[client_index].message(":server 461 " + clients[client_index].getNickname() + " MODE :Not enough parameters\r\n");
+                    clients[client_index].message(":server 461 " + clients[client_index].getNickname() + " MODE :Useage MODE <channel> -o <nickname>\r\n");
+                    return;
+                }
+
+                std::vector<Client> channel_ops = it->getOperators();
+                for (std::vector<Client>::iterator op_it = channel_ops.begin(); op_it != channel_ops.end(); ++op_it)
+                {
+                    if (op_it->getNickname() == parameter)
+                    {
+                        it->removeOperator(*op_it);
+                        break;
+                    }
+                }
+                mode_message = ":" + clients[client_index].getNickname() + "!" + clients[client_index].getUsername() + "@" + clients[client_index].getIp_address() + " MODE " + channel_name + " -o " + parameter + "\r\n";
+            }
+            else if (mode == "+k")
+            {
+                if (parameter.empty())
+                {
+                    clients[client_index].message(":server 461 " + clients[client_index].getNickname() + " MODE :Not enough parameters\r\n");
+                    clients[client_index].message(":server 461 " + clients[client_index].getNickname() + " MODE :Useage MODE <channel> +k <key>\r\n");
+                    return;
+                }
+
+                it->setKey(parameter);
+                it->setChannelKey(true);
+                mode_message = ":" + clients[client_index].getNickname() + "!" + clients[client_index].getUsername() + "@" + clients[client_index].getIp_address() + " MODE " + channel_name + " +k " + parameter + "\r\n";
+            }
+            else if (mode == "-k")
+            {
+                it->setKey("");
+                it->setChannelKey(false);
+                mode_message = ":" + clients[client_index].getNickname() + "!" + clients[client_index].getUsername() + "@" + clients[client_index].getIp_address() + " MODE " + channel_name + " -k\r\n";
+            }
+            else
+            {
+                clients[client_index].message(":server 472 " + clients[client_index].getNickname() + " " + mode + " :Unknown MODE flag\r\n");
+                return;
+            }
+
+            // Broadcast mode change to all channel clients
+            for (std::vector<Client>::iterator client_it = channel_clients.begin(); client_it != channel_clients.end(); ++client_it)
+            {
+                client_it->message(mode_message);
+            }
+            return;
+        }
+    }
+    
+    if (!channel_found)
+    {
+        clients[client_index].message(":server 403 " + clients[client_index].getNickname() + " " + channel_name + " :No such channel\r\n");
+    }
 }
-
